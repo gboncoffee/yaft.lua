c31daecd1fff305ff3beb0a3072402417c6d285b
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2022-12-12 23:40:29 -0300

    Added config with exe opener

--- plugin/init.lua
+++ plugin/init.lua
@@ -8,7 +8,27 @@ end
 
 g.autoloaded_yaft = true
 
-require "yaft".setup_keys(require "yaft".default_keys())
+vim.cmd [[
+hi link YaftDir Directory
+hi link YaftExe Character
+hi link YaftLnk Question
+hi link YaftRoot Todo
+]]
+
+-- defaults
+g._yaft_config = {
+    yaft_exe_opener = function(entry, fullpath)
+        local has_run, run = pcall(require, "run")
+        if has_run then
+            run.run(fullpath)
+            return
+        end
+        v.nvim_win_call(require "yaft"._get_first_usable_window(), function()
+            vim.cmd("split | term " .. fullpath)
+        end)
+    end,
+    keys = require "yaft".default_keys()
+}
 
 command("YaftToggle", function(args)
     require "yaft".toggle_yaft(args.args)
